144ea960b9e80779e6a84925d39dea888cc34e77
Ryokenka <remi.guerin99@gmail.com>
2024-11-06 15:01:24 +0100

    first commit

---
+++ Sudoku.DlxSolver/DlxSolver.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Sudoku.Shared;
+namespace Sudoku.DlxSolver;
+
+public class DlxSolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+		    {
+			    return s;
+		    }
+}
