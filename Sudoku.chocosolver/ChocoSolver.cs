bcb90641a79b3289c37be2f799e6096bea295433
ewarskotte <elea.warskotte@epfedu.fr>
2024-11-27 17:22:04 +0100

    solver fini avec chemins absolus

--- Sudoku.chocosolver/ChocoSolver.cs
+++ Sudoku.chocosolver/ChocoSolver.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using Newtonsoft.Json; // Assurez-vous d'installer le package NuGet Newtonsoft.Json
+using Newtonsoft.Json; 
 using Sudoku.Shared;
 
 namespace Sudoku.Solvers
@@ -11,13 +11,12 @@ namespace Sudoku.Solvers
         public SudokuGrid Solve(SudokuGrid sudokuGrid)
         {
             string pythonPath = @"C:\Users\elea1\AppData\Local\Programs\Python\Python313\Python.exe"; // Chemin vers l'exécutable Python
-            string scriptPath = @"Sudoku.chocosolver\PythonChocoSolver.py"; // Chemin vers le script Python
+            string scriptPath = @"C:\Users\elea1\Desktop\Code\5A_IA\Sudoku\Sudoku.chocosolver\PythonChocoSolver.py";; // Chemin vers le script Python
 
-            // Convertir la grille en JSON
+           
             var inputGrid = new { sudoku = sudokuGrid.Cells };
             string inputJson = JsonConvert.SerializeObject(inputGrid);
 
-            // Configurer le processus
             var processStartInfo = new ProcessStartInfo
             {
                 FileName = pythonPath,
@@ -34,17 +33,11 @@ namespace Sudoku.Solvers
                 using (var process = new Process { StartInfo = processStartInfo })
                 {
                     process.Start();
-
-                    // Écrire les données JSON dans l'entrée standard
                     using (var writer = process.StandardInput)
                     {
                         writer.Write(inputJson);
                     }
-
-                    // Lire la sortie standard
                     string outputJson = process.StandardOutput.ReadToEnd();
-
-                    // Attendre la fin du processus
                     process.WaitForExit();
 
                     if (process.ExitCode != 0)
@@ -52,8 +45,6 @@ namespace Sudoku.Solvers
                         string error = process.StandardError.ReadToEnd();
                         throw new Exception($"Python script error: {error}");
                     }
-
-                    // Désérialiser la sortie JSON
                     var result = JsonConvert.DeserializeObject<dynamic>(outputJson);
                     int[,] solution = JsonConvert.DeserializeObject<int[,]>(result.solution.ToString());
                     return new SudokuGrid { Cells = solution };
