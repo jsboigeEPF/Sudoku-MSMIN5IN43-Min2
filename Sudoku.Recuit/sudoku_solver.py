3ca4157433d407a67d09aff8b10f6513ff06f456
Edmaruu <mamboedwinpau.inchaud@epfedu.fr>
2024-12-03 10:06:18 +0100

    ajout de la grille du benchmark pour résoudre avec notre fonction de côut

--- Sudoku.Recuit/sudoku_solver.py
+++ Sudoku.Recuit/sudoku_solver.py
@@ -12,19 +12,7 @@ def solveSudokuWrapper(input_sudoku):
  
 
 
-startingSudoku = """
-                    024007000
-                    600000000
-                    003680415
-                    431005000
-                    500000032
-                    790000060
-                    209710800
-                    040093000
-                    310004750
-                """
-
-sudoku = np.array([[int(i) for i in line] for line in startingSudoku.split()])
+sudoku = sudoku_grid
 
 def PrintSudoku(sudoku):
     print("\n")
@@ -187,5 +175,4 @@ def solveSudoku (sudoku):
     return(tmpSudoku)
 
 solution = solveSudokuWrapper(sudoku)
-print(CalculateNumberOfErrors(solution))
 PrintSudoku(solution)
